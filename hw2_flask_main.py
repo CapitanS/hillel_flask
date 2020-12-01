e840d501ff2ea4ba3d92fb64ab7048e4f06a1813
dimat <dimaprizma2016@gmail.com>
2020-12-01 16:00:30 +0200

    Finished task 1. Return information from requirements.txt

---
+++ hw2_flask_main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Flask!'
+
+
+@app.route('/requirements/')
+def get_requirements():
+    with open('requirements.txt') as r:
+        requirements = r.readlines()
+        requirements = '; '.join([i.strip() for i in requirements])
+    return requirements
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
